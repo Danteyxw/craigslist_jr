4d511ef363dca0b5c2ebd3d3454a6884f9c6cef9
wengkhing <kheryee@live.com>
2015-08-25 18:41:53 +0800

    Implement edit functions

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,7 @@
 get '/' do
   # Look in app/views/index.erb
+  session = nil
+  session = {}
   erb :index
 end
 
@@ -11,28 +13,43 @@ post '/posts' do
 end
 
 post '/create_post' do
-  Post.create(title: params[:title], category_id: Category.find_by(name: session[:category]).id, description: params[:description], email: params[:email], price: params[:price], key: Faker::Internet.password(8))
-
-  redirect '/post'
-end
-
-post '/editor_create' do
+  key = Faker::Internet.password(8)
+  Post.create(title: params[:title],
+              category_id: Category.find_by(name: params[:category]).id,
+              description: params[:description],
+              email: params[:email],
+              price: params[:price],
+              key: key)
+  session[:key] = key
   session[:category] = params[:category]
-
-  redirect '/editor'
+  session[:posts] = Category.find_by(name: session[:category]).posts
+  redirect '/key'
 end
 
 post '/edit_post' do
+  session[:post] = Post.find_by(key: params[:key]).id
+  redirect '/edit'
+end
+
+post '/save_post' do
+  Post.update(title: params[:title],
+              description: params[:description],
+              email: params[:email],
+              price: params[:price])
+  session[:category] = params[:category]
+  session[:posts] = Category.find_by(name: session[:category]).posts
+  redirect '/posts'
 end
 
 get '/posts' do
   erb :posts
 end
 
-get '/editor' do
-  erb :editor
+get '/key' do
+  erb :key
 end
 
-get '/post' do
-  erb :post
+get '/edit' do
+  @post = Post.find(session[:post])
+  erb :edit
 end
