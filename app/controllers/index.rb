0ec5135e65d1d409a0cccca816fbbfff16a51197
wengkhing <kheryee@live.com>
2015-08-25 11:15:22 +0800

    implements main page and categorical posts

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,9 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/posts' do
+  @category = params[:category]
+  @posts = Category.find_by(name: @category).posts
+  erb :posts
+end
